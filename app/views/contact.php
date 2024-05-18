06f1b4ef4e09ab02607735ea4a7e859ef264467b
Ranshari Rodrigo <ransharirodrigo@gmail.com>
2024-05-18 23:42:44 +0530

    Get in touch form created in contact page

--- app/views/contact.php
+++ app/views/contact.php
@@ -34,6 +34,60 @@
             </div>
 
 
+            <div class="col-12 contactPageContentDiv">
+                <div class="row h-100 d-flex justify-content-center justify-content-md-around ">
+
+                    <!-- Get In Touch Div -->
+                    <div class="col-10 col-md-6">
+                        <div class="row">
+                            <div>
+                                <h1><span class="text-white">Get In&nbsp;</span><span class="contactPageHeadingTxt"> Touch.</span></h1>
+                            </div>
+                            <div class="col-12 py-4 px-3 contactFormDiv mt-2">
+
+                                <!-- Name -->
+                                <div class="row">
+                                    <div class="col-12 col-sm-4">
+                                        <p class="text-white">Your Name ?</p>
+                                    </div>
+                                    <div class="col-12 col-sm-8">
+                                        <input type="text" class="form-control" />
+                                    </div>
+                                </div>
+
+                                <!-- Email -->
+                                <div class="row mt-4">
+                                    <div class="col-12 col-sm-4">
+                                        <p  class="text-white">Your Email ?</p>
+                                    </div>
+                                    <div class="col-12 col-sm-8">
+                                        <input type="text" class="form-control" />
+                                    </div>
+                                </div>
+
+                                <!-- Message -->
+                                <div class="row mt-4">
+                                    <div class="col-12 col-sm-4">
+                                        <p  class="text-white">Your Message ?</p>
+                                    </div>
+                                    <div class="col-12 col-sm-8">
+                                        <textarea rows="6" class="form-control"></textarea>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <!-- Connect with Me Online Div -->
+                    <div class="col-10 col-md-5">
+
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     </div>
 </body>
