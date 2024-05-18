2fc3a94775c5707c21a79dcd12a080363bfb389a
Ranshari Rodrigo <ransharirodrigo@gmail.com>
2024-05-18 09:14:26 +0530

    Nav bar created

---
+++ includes/NavBar.php
@@ -0,0 +1,11 @@
+<div class="row d-flex flex-row justify-content-end py-4 h-100">
+    <div class="col-2 col-lg-1 text-end">
+        <a href="" class="text-white text-decoration-none">Portfolio</a>
+    </div>
+    <div class="col-2 col-lg-1 text-end">
+        <a href="" class="text-white text-decoration-none ">About</a>
+    </div>
+    <div class="col-2 col-lg-1 text-end">
+        <a href="" class="text-white text-decoration-none  ">Contact</a>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+
+
+    <link rel="stylesheet" href="assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <div class="container-fluid vh-100 ">
+        <div class="row px-5 h-100">
+
+            <!-- Offcanvas icon -->
+            <div class="col-12  navBarDiv d-md-none ">
+                <div class="row h-100 d-flex align-items-center">
+                    <i class="bi bi-list text-white fs-5"></i>
+                </div>
+            </div>
+
+            <!-- Nav Bar -->
+            <div class="col-12 navBarDiv d-none d-md-block">
+                <?php
+                include("includes/NavBar.php");
+                ?>
+            </div>
+
+            <div class="col-12 indexPageContentDiv">
+                <div class="row h-100 d-flex align-items-center">
+
+                    <div class="col-12">
+                        <div class="row">
+
+                            <!-- Name and stack div-->
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-12 col-md-8 text-center text-md-start">
+ww
+                                    </div>
+                                    <div class="col-12 col-md-4 text-center text-md-start">
+ w
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!-- Circle div -->
+                            <div class="col-4">
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
